c6736b6e78009501c2f5d5180f74f54ce6d9a2c2
MaksimBorovets <77835944+MaksimBorovets@users.noreply.github.com>
2021-02-26 12:29:03 +0200

    first commit

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+const API_KEY = '1e34265676eecf0c85d3db76f6e4ae87';
+
+const requests = {
+	fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+	fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_network=123`,
+	fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+	fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+	fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+	fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+	fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+	fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+};
+
+export default requests;
+
+
