7b6725e23593383e76eeb8b2249a927030a460f1
MaksimBorovets <77835944+MaksimBorovets@users.noreply.github.com>
2021-02-26 12:36:41 +0200

    Updates

---
+++ precache-manifest.083c8ee45e678ea6f5f7278443a65174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1285c30f0d7541226da4b4ac10425b3",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "fcb273a7df1e4b9e3245",
+    "url": "/netflix-clone/static/css/main.e24c6389.chunk.css"
+  },
+  {
+    "revision": "d94122286c2ed7c2735b",
+    "url": "/netflix-clone/static/js/2.79c20a69.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/netflix-clone/static/js/2.79c20a69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcb273a7df1e4b9e3245",
+    "url": "/netflix-clone/static/js/main.550227cd.chunk.js"
+  },
+  {
+    "revision": "b948bf8b867562944125",
+    "url": "/netflix-clone/static/js/runtime-main.66b8f367.js"
+  }
+]);
